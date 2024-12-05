19771022f7eac65e1dd6fec3f1a24a2a0927a6ba
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-05 13:05:50 +0800

    Add query models.

--- invento/inventory/models.py
+++ invento/inventory/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+
+from pymongo import MongoClient
+
+
+def get_db_handle(db_name, host, port, username, password):
+
+    client = MongoClient(
+        host=host, port=int(port), username=username, password=password
+    )
+    db_handle = client[db_name]
+    return client
+
+
+def get_collection(collectionName):
+    client = get_db_handle("invento", "localhost", "27017", "emman-pip", "108996")
+    return client[collectionName]
