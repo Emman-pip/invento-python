aa386d82a617dd5c5ad380fd86fdce30f50f62eb
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-05 14:43:14 +0800

    Update sign up view as index page.

--- invento/inventory/views.py
+++ invento/inventory/views.py
@@ -1,8 +1,29 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+from . import models
+
+from . import forms
+
 
 def index(request):
-    return render(request, "inventory/index.html")
+    if request.method == "POST":
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            organization = form.cleaned_data["organization"]
+            password = form.cleaned_data["password"]
+            confirm = form.cleaned_data["confirmPassword"]
+            if password != confirm:
+                return render(
+                    request,
+                    "inventory/index.html",
+                    {"form": forms.SignUpForm, "warning": "please check all fields"},
+                )
+            return HttpResponse(username + email + organization + password)
+    data = models.get_collection("Inventories")
+    return render(request, "inventory/index.html", {"form": forms.SignUpForm})
